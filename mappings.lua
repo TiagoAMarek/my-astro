859054a672b76b8c36e52ea44edd615cba562b16
TiagoAMarek <tiagomarek@gmail.com>
2023-10-06 17:34:40 -0300

    add new keymaps

--- mappings.lua
+++ mappings.lua
@@ -6,12 +6,27 @@
 return {
   -- first key is the mode
   n = {
-      ["<leader>b"] = false,
-      ["<leader>bb"] = false,
-      ["<leader>bd"] = false,
-      ["<leader>b\\"] = false,
-      ["<leader>b|"] = false,
-     ["<leader><leader>"] = { function() require("telescope.builtin").find_files() end, desc = "Find files" },
+    -- Disabled because of tabline
+    ["<leader>b"] = false,
+    ["<leader>bb"] = false,
+    ["<leader>bd"] = false,
+    ["<leader>b\\"] = false,
+    ["<leader>b|"] = false,
+
+    ["<C-,>"] = { function() require("telescope.builtin").find_files() end, desc = "Find files" },
+    ["<C-.>"] = {
+      function() require("telescope.builtin").find_files { hidden = true, no_ignore = true } end,
+      desc = "Find all files",
+    },
+    ["<C-9>"] = { function() require("telescope.builtin").live_grep() end, desc = "Find words" },
+    ["<C-0>"] = {
+      function()
+        require("telescope.builtin").live_grep {
+          additional_args = function(args) return vim.list_extend(args, { "--hidden", "--no-ignore" }) end,
+        }
+      end,
+      desc = "Find words in all files",
+    }
     -- navigate buffer tabs with `H` and `L`
     -- L = {
     --   function() require("astronvim.utils.buffer").nav(vim.v.count > 0 and vim.v.count or 1) end,
--- plugins/harpoon.lua
+++ plugins/harpoon.lua
@@ -21,7 +21,7 @@ return {
       { "<C-e>", function() require("harpoon.ui").toggle_quick_menu() end, desc = "Toggle quick menu" },
       { "<C-p>", function() require("harpoon.ui").nav_prev() end, desc = "Goto previous mark" },
       { "<C-n>", function() require("harpoon.ui").nav_next() end, desc = "Goto next mark" },
-      { prefix .. "m", "<cmd>Telescope harpoon marks<CR>", desc = "Show marks in Telescope" },
+      { "<C-b>", "<cmd>Telescope harpoon marks<CR>", desc = "Show marks in Telescope" },
     },
   },
   {
