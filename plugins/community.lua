822751edde72c1e1f18bc8db8df883d330780102
TiagoAMarek <tiagomarek@gmail.com>
2023-10-06 12:01:40 -0300

    update configs

--- plugins/community.lua
+++ plugins/community.lua
@@ -4,6 +4,13 @@ return {
   -- example of importing a plugin, comment out to use it or add your own
   -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
 
-  -- { import = "astrocommunity.colorscheme.catppuccin" },
+  { import = "astrocommunity.bars-and-lines.heirline-vscode-winbar" },
+  { import = "astrocommunity.colorscheme.catppuccin" },
+  { import = "astrocommunity.colorscheme.github-nvim-theme" },
+  { import = "astrocommunity.editing-support.rainbow-delimiters-nvim" },
+  { import = "astrocommunity.editing-support.treesj" },
+  { import = "astrocommunity.motion.harpoon" },
+  { import = "astrocommunity.motion.hop-nvim" },
+  { import = "astrocommunity.pack.typescript" },
   -- { import = "astrocommunity.completion.copilot-lua-cmp" },
 }
