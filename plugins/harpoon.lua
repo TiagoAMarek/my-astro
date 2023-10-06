c71f8c99e2393f0c3b273b08b38226fdca03b130
TiagoAMarek <tiagomarek@gmail.com>
2023-10-06 16:57:17 -0300

    hide tabline and update harpoon

--- plugins/harpoon.lua
+++ plugins/harpoon.lua
@@ -1,4 +1,4 @@
-local prefix = "<leader><leader>"
+local prefix = "<leader>b"
 local term_string = vim.fn.exists "$TMUX" == 1 and "tmux" or "terminal"
 local maps = { n = {} }
 local icon = vim.g.icons_enabled and "󱡀 " or ""
@@ -14,30 +14,14 @@ return {
     cmd = { "Harpoon" },
     keys = {
       { "<C-a>", function() require("harpoon.mark").add_file() end, desc = "Add file" },
+      { prefix .. "a", function()
+        require("harpoon.mark").add_file()
+        require("astronvim.utils").notify(("File Harpooned"):format(astronvim.default_colorscheme), vim.log.levels.INFO)
+      end, desc = "Add file" },
       { "<C-e>", function() require("harpoon.ui").toggle_quick_menu() end, desc = "Toggle quick menu" },
-      {
-        "<C-x>",
-        function()
-          vim.ui.input({ prompt = "Harpoon mark index: " }, function(input)
-            local num = tonumber(input)
-            if num then require("harpoon.ui").nav_file(num) end
-          end)
-        end,
-        desc = "Goto index of mark",
-      },
       { "<C-p>", function() require("harpoon.ui").nav_prev() end, desc = "Goto previous mark" },
       { "<C-n>", function() require("harpoon.ui").nav_next() end, desc = "Goto next mark" },
       { prefix .. "m", "<cmd>Telescope harpoon marks<CR>", desc = "Show marks in Telescope" },
-      {
-        prefix .. "t",
-        function()
-          vim.ui.input({ prompt = term_string .. " window number: " }, function(input)
-            local num = tonumber(input)
-            if num then require("harpoon." .. term_string).gotoTerminal(num) end
-          end)
-        end,
-        desc = "Go to " .. term_string .. " window",
-      },
     },
   },
   {
